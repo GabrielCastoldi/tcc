d6205f8501f36e31032efcd1f6d7c3159ed8d80b
Riuchek <riuchek@gmail.com>
2025-10-14 15:42:38 -0300

    chore: cadastro on page

--- site/src/pages/calendarioVacinacao.jsx
+++ site/src/pages/calendarioVacinacao.jsx
@@ -1,23 +1,43 @@
 
 import '../css/CalendarioVacinacao.css';
 import { useNavigate, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 export default function CalendarioVacinacao() {
-
+  const [nomeVacina, setNomeVacina] = useState('');
+  const [lote, setLote] = useState('');
+  const [doseAplicada, setDoseAplicada] = useState('');
+  const [posto, setPosto] = useState('');
+  const [proximaData, setProximaData] = useState('');
+  const [dataAtual, setDataAtual] = useState('');
   const navigate = useNavigate();
   const { cpf } = useParams();
 
-  console.log('CPF da URL:', cpf);
+  console.log(cpf);
+
+  useEffect(() => {
+    const hoje = new Date();
+    const dataFormatada = hoje.toLocaleDateString('pt-BR');
+    setDataAtual(dataFormatada);
+  }, []);
+
   const handleTrocarPaciente = () => {
     navigate('/cpf');
   };
 
-  const handleNovaVacinacao = () => {
-    navigate('/cadastro-vacinacao');
-  };
-
   const handleSair = () => {
     navigate('/');
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      nomeVacina,
+      lote,
+      doseAplicada,
+      posto,
+      proximaData
+    });
   };
 
   return (
@@ -33,11 +53,62 @@ export default function CalendarioVacinacao() {
 
       <div className="calendario-titulo">
         <div className="calendario-esquerda">
-          <h1>CALENDÁRIO DE VACINAÇÃO</h1>
+          <h1>CADASTRAR VACINAÇÃO</h1>
+          <h3>DATA DE VACINAÇÃO: {dataAtual}</h3>
 
-          <button className="btn-nova-vacinacao" onClick={handleNovaVacinacao}>
-            NOVA VACINAÇÃO
-          </button>
+          <form onSubmit={handleSubmit} className="form-vacinacao">
+          <div className="input-vacinacao">
+            <input
+              type="text"
+              placeholder="Nome da vacina"
+              value={nomeVacina}
+              onChange={(e) => setNomeVacina(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="input-vacinacao">
+            <input
+              type="text"
+              placeholder="Lote"
+              value={lote}
+              onChange={(e) => setLote(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="input-vacinacao">
+            <input
+              type="number"
+              placeholder="Dose aplicada"
+              value={doseAplicada}
+              onChange={(e) => setDoseAplicada(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="input-vacinacao">
+            <input
+              type="text"
+              placeholder="Posto de aplicação"
+              value={posto}
+              onChange={(e) => setPosto(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="input-vacinacao">
+            <label> Próxima data de vacinação</label>
+            <input
+              type="date"
+              value={proximaData}
+              onChange={(e) => setProximaData(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn-vacinacao">CADASTRAR</button>
+        </form>
         </div>
 
         <div className="calendario-direita">
