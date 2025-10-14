8f911a7762ffd3dd5294edb666508ef4ae8a48ab
Riuchek <riuchek@gmail.com>
2025-10-14 15:35:21 -0300

    fix: change post to get

--- backend/src/routes/getPatient.js
+++ backend/src/routes/getPatient.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const getPatientsController = require('../controllers/getPatients');
 
 // Rota para encontrar o paciente pelo CPF
-router.post('/encontrar-paciente/:cpf', getPatientsController.encontrarPaciente);
+router.get('/encontrar-paciente/:cpf', getPatientsController.encontrarPaciente);
 
 module.exports = router;
